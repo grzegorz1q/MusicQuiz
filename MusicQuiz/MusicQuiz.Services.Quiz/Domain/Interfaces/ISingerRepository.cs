21b8c0a3e3459114558c5f7ec1374fe10dd83491
grzegorz1q <grzegorbaryla@gmail.com>
2025-11-19 16:19:57 +0100

    implementing new functions

---
+++ MusicQuiz/MusicQuiz.Services.Quiz/Domain/Interfaces/ISingerRepository.cs
@@ -0,0 +1,9 @@
+﻿using MusicQuiz.Services.Quiz.Domain.Model;
+
+namespace MusicQuiz.Services.Quiz.Domain.Interfaces
+{
+    public interface ISingerRepository
+    {
+        Task<IEnumerable<Singer>> GetRandomSingersAsync(int amount);
+    }
+}
---
+++ MusicQuiz/MusicQuiz.Services.Quiz/Infrastructure/Repositories/SingerRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MusicQuiz.Services.Quiz.Domain.Interfaces;
+using MusicQuiz.Services.Quiz.Domain.Model;
+using MusicQuiz.Services.Quiz.Infrastructure.Persistence;
+
+namespace MusicQuiz.Services.Quiz.Infrastructure.Repositories
+{
+    public class SingerRepository : ISingerRepository
+    {
+        private readonly QuizDbContext _context;
+        public SingerRepository(QuizDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Singer>> GetRandomSingersAsync(int amount)
+        {
+            return await _context.Singers.OrderBy(x => Guid.NewGuid()).Take(amount).ToListAsync();
+        }
+    }
+}
